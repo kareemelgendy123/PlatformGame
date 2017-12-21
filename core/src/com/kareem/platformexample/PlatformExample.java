33a618793cdf181c8e2475abad136166a922ccb5
Kareem <Kareem@172.21.49.231>
2017-12-21 09:51:50 -0500

    Platform Example

--- core/src/com/kareem/platformexample/PlatformExample.java
+++ core/src/com/kareem/platformexample/PlatformExample.java
@@ -1,5 +1,6 @@
 package com.kareem.platformexample;
 
+
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
@@ -7,28 +8,39 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+// we are extending game instead of ApplicationAdapter
+// this lets of use some of the screen management tools
 public class PlatformExample extends Game {
-        SpriteBatch batch;
-	Texture img;
+        // we only want 1 sprite batch for the game
+        // here it is!
+	private SpriteBatch batch;
+
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+            // initialize the sprite batch
+            batch = new SpriteBatch();
+            // create the main game screen we want to use
+            MainGame game = new MainGame(this);
+            // set the screen to show it
+            this.setScreen(game);
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+            // this will automatically call render on the screen that is set
+            super.render();
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+            // get rid of heavy variables
+            batch.dispose();
 	}
+        
+        public SpriteBatch getBatch(){
+            // returns the spritebatch so other screens can use it
+            return batch;
+        }
+        
 }
--- desktop/src/com/kareem/platformexample/desktop/DesktopLauncher.java
+++ desktop/src/com/kareem/platformexample/desktop/DesktopLauncher.java
@@ -2,10 +2,19 @@ package com.kareem.platformexample.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.tools.texturepacker.TexturePacker;
+import com.badlogic.gdx.tools.texturepacker.TexturePacker.Settings;
 import com.kareem.platformexample.PlatformExample;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
+            
+                Settings settings = new Settings();
+                settings.maxHeight = 1024;
+                settings.maxWidth = 1024;
+                
+                TexturePacker.process("raw", "packed", "player");
+                
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		new LwjglApplication(new PlatformExample(), config);
 	}
